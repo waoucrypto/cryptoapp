84c6dbddb0540d6ff2209a8433bee6ee743d7210
nicobullerouge <nicoduguet@gmail.com>
2023-12-07 16:25:52 +0100

    sync

--- public/storm2/bot/index.php
+++ public/storm2/bot/index.php
@@ -26,19 +26,19 @@ $(document).ready(function() {
         $(".bot_mobile a:nth-child(1)").click(function(event) {
             event.preventDefault(); 
             $('.bot_left').show().animate({"left":"0"}, 200);
-            $('.bot_bottom').animate({"left":"75%", "right":"-75%"}, 200);
-            $('.bot_empty').animate({"left":"75%", "right":"-75%"}, 200);
-            $('.bot_center').animate({"left":"75%", "right":"-75%"}, 200);
-            $('.bot_mobile').animate({"left":"75%", "right":"-75%"}, 200);
+            $('.bot_bottom').animate({"left":"75%", "right":"-75%","opacity":0.2}, 200);
+            $('.bot_empty').animate({"left":"75%", "right":"-75%","opacity":0.2}, 200);
+            $('.bot_center').animate({"left":"75%", "right":"-75%","opacity":0.2}, 200);
+            $('.bot_mobile').animate({"left":"75%", "right":"-75%","opacity":0.2}, 200);
             event.stopPropagation(); 
         });
 
         $(document).click(function() {
             $('.bot_left').animate({"left":"-75%"}, 200).delay().slideDown().fadeOut();
-            $('.bot_bottom').animate({"left":"0", "right":"0"}, 200);
-            $('.bot_empty').animate({"left":"0", "right":"0"}, 200);
-            $('.bot_center').animate({"left":"0", "right":"0"}, 200);
-            $('.bot_mobile').animate({"left":"0", "right":"0"}, 200);
+            $('.bot_bottom').animate({"left":"0", "right":"0","opacity":1}, 200)
+            $('.bot_empty').animate({"left":"0", "right":"0","opacity":1}, 200)
+            $('.bot_center').animate({"left":"0", "right":"0","opacity":1}, 200)
+            $('.bot_mobile').animate({"left":"0", "right":"0","opacity":1}, 200)
         });
 
         $('.bot_left').click(function(event) {      
