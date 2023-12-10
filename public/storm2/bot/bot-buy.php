953354f1c72f509757ba96976b6e18614524c335
nicobullerouge <nicoduguet@gmail.com>
2023-12-10 15:56:48 +0100

    sync

--- public/storm2/bot/bot-buy.php
+++ public/storm2/bot/bot-buy.php
@@ -13,35 +13,45 @@ $PEPE
 
 <div class="bot_token2">
 
-<?php include '../dashboard/dash-buy.php' ;?>
-
-<div class="bot_token">
-<ul>
-<li>
-<div> 
+<div class="bot_token3">
+<div class="bot_token_ico"> 
 <img src="/images/pepe.jpg">
 <em>
 <u>PEPE</u>
 <u>PEPE / ETH</u>
 </em>
 </div>
-</li> 
-<li><i class="fa-regular fa-file"></i> &nbsp;Contract: <b>0x698...1933</b></li>
-<li><i class="fa-solid fa-building-columns"></i> &nbsp;Pool: <b>0x1195...7b58</b></li>
-<li><i class="fa-solid fa-shield-halved"></i> &nbsp;Score: <b>75/100</b></li>
-<li><i class="fa-solid fa-chart-line"></i> &nbsp;Chart: <b><i class="fa-solid fa-link"></i> <a>View</a></b></li>
+<?php include '../dashboard/dash-buy.php' ;?>
+</div>
+
+<div class="bot_token">
+<ul>
+<li>Overview</li>
+<div class="bot_token_ico"> 
+<img src="/images/pepe.jpg">
+<em>
+<u>PEPE</u>
+<u>PEPE / ETH</u>
+</em>
+</div>
+<li><i class="fa-regular fa-file"></i> &nbsp;Contract: <b>0x698...1933 <i class="fa-regular fa-copy"></i></b></li>
+<li><i class="fa-solid fa-building-columns"></i> &nbsp;Pool: <b>0x1195...7b58 <i class="fa-regular fa-copy"></i></b></li>
+<li><i class="fa-solid fa-chart-line"></i> &nbsp;Chart: <b><a>View</a> <i class="fa-solid fa-link"></i> </b></li>
+<li></li>
+<li><i class="fa-solid fa-shield-halved"></i> &nbsp;Score: <b>75/100 <i class="fa-solid fa-circle-check"></i></b></li>
 <li></li>
 <li><i class="fa-solid fa-sack-dollar"></i> &nbsp;Market Cap: <b>$527,834,919</b></li>
-<li><i class="fa-solid fa-water"></i> &nbsp;Liquidity: <b>$10.70M <i class="fa-solid fa-lock"></i> (13 months) <i class="fa-solid fa-circle-check"></i> </b></li>
+<li><i class="fa-solid fa-water"></i> &nbsp;Liquidity: <b>$10.70M <i class="fa-solid fa-lock"></i> (13 months) <i class="fa-solid fa-circle-check"></i></b></li>
 <li><i class="fa-regular fa-gem"></i> &nbsp;Supply: <b>413.77T</b></li>
 <li><i class="fa-solid fa-users"></i> &nbsp;Holders: <b>147.03K</b></li>
 <li></li>
+<li><i class="fa-solid fa-coins"></i> &nbsp;Price: <b>$0.00000113</b></li>
 <li><i class="fa-solid fa-rocket"></i> &nbsp;1H: <b><em class="red">-1.41%</em> <e>VOL</e> $53.56K <e>TXS</e> 32</b></li>
 <li><i class="fa-solid fa-chart-simple"></i> &nbsp;24H: <b><em class="red">-2.99%</em> <e>VOL</e> $1.86M <e>TXS</e> 839</b></li>
 <li></li>
-<li><i class="fa-solid fa-toilet"></i> &nbsp;Honeypot: <b><i class="fa-solid fa-circle-check"></i> No</b></li>
-<li><i class="fa-solid fa-file-contract"></i> &nbsp;Renounced: <b><i class="fa-solid fa-circle-check"></i> Yes</b></li>
-<li><i class="fa-solid fa-scale-balanced"></i> &nbsp;Tax: <b><i class="fa-solid fa-circle-check"></i> 🅑 0% 🅢 0%</b></li>
+<li><i class="fa-solid fa-toilet"></i> &nbsp;Honeypot: <b>No <i class="fa-solid fa-circle-check"></i> </b></li>
+<li><i class="fa-solid fa-file-contract"></i> &nbsp;Renounced: <b>Yes <i class="fa-solid fa-circle-check"></i> </b></li>
+<li><i class="fa-solid fa-scale-balanced"></i> &nbsp;Tax: <b>🅑 0% 🅢 0% <i class="fa-solid fa-circle-check"></i></b></li>
 <li></li>
 <li><i class="fa-regular fa-calendar"></i> &nbsp;Pool created: <b>6 months ago</b></li>
 </ul>
