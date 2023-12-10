829b6cf451b713697935b99a30e3e3c810933b68
nicobullerouge <nicoduguet@gmail.com>
2023-12-10 15:19:54 +0100

    sync

--- public/storm2/bot/bot-buy.php
+++ public/storm2/bot/bot-buy.php
@@ -29,19 +29,19 @@ $PEPE
 <li><i class="fa-regular fa-file"></i> &nbsp;Contract: <b>0x698...1933</b></li>
 <li><i class="fa-solid fa-building-columns"></i> &nbsp;Pool: <b>0x1195...7b58</b></li>
 <li><i class="fa-solid fa-shield-halved"></i> &nbsp;Score: <b>75/100</b></li>
-<li><i class="fa-solid fa-chart-line"></i> &nbsp;Chart: <b><a>View</a></b></li>
+<li><i class="fa-solid fa-chart-line"></i> &nbsp;Chart: <b><i class="fa-solid fa-link"></i> <a>View</a></b></li>
 <li></li>
 <li><i class="fa-solid fa-sack-dollar"></i> &nbsp;Market Cap: <b>$527,834,919</b></li>
-<li><i class="fa-solid fa-water"></i> &nbsp;Liquidity: <b>$10.70M <i class="fa-solid fa-lock"></i> (13 months)</b></li>
+<li><i class="fa-solid fa-water"></i> &nbsp;Liquidity: <b>$10.70M <i class="fa-solid fa-lock"></i> (13 months) <i class="fa-solid fa-circle-check"></i> </b></li>
 <li><i class="fa-regular fa-gem"></i> &nbsp;Supply: <b>413.77T</b></li>
 <li><i class="fa-solid fa-users"></i> &nbsp;Holders: <b>147.03K</b></li>
 <li></li>
-<li><i class="fa-solid fa-rocket"></i> &nbsp;Vol. 1H: <b><em class="red">-1.41%</em> $53.56K 🅑 8 TXS 🅢 19 TXS</b></li>
-<li><i class="fa-solid fa-chart-simple"></i> &nbsp;Vol. 24H: <b><em class="red">-2.99%</em> $1.86M 🅑 439 TXS 🅢 365 TXS</b></li>
+<li><i class="fa-solid fa-rocket"></i> &nbsp;1H: <b><em class="red">-1.41%</em> <e>VOL</e> $53.56K <e>TXS</e> 32</b></li>
+<li><i class="fa-solid fa-chart-simple"></i> &nbsp;24H: <b><em class="red">-2.99%</em> <e>VOL</e> $1.86M <e>TXS</e> 839</b></li>
 <li></li>
-<li><i class="fa-solid fa-toilet"></i> &nbsp;Honeypot: <b><i class="fa-solid fa-square-check"></i> No</b></li>
-<li><i class="fa-solid fa-file-contract"></i> &nbsp;Renounced: <b><i class="fa-solid fa-square-check"></i> Yes</b></li>
-<li><i class="fa-solid fa-scale-balanced"></i> &nbsp;Tax: <b>🅑 0% 🅢 0%</b></li>
+<li><i class="fa-solid fa-toilet"></i> &nbsp;Honeypot: <b><i class="fa-solid fa-circle-check"></i> No</b></li>
+<li><i class="fa-solid fa-file-contract"></i> &nbsp;Renounced: <b><i class="fa-solid fa-circle-check"></i> Yes</b></li>
+<li><i class="fa-solid fa-scale-balanced"></i> &nbsp;Tax: <b><i class="fa-solid fa-circle-check"></i> 🅑 0% 🅢 0%</b></li>
 <li></li>
 <li><i class="fa-regular fa-calendar"></i> &nbsp;Pool created: <b>6 months ago</b></li>
 </ul>
